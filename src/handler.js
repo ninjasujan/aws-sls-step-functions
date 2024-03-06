7e539e5ba956536b2eab13ce3c83dc114a51b4f3
sujan <sujanb.poojary@gmail.com>
2024-03-06 17:05:51 +0530

    Fix: Passing state bw state

--- src/handler.js
+++ src/handler.js
@@ -1,26 +1,31 @@
 /** Lambda step function Task */
 const checkCandidateAvailability = async (event) => {
     const count = event.count;
-    return {
-        count,
-    };
+    const isCandidateExist = count > 0;
+    return isCandidateExist;
 };
 
 /** Lambda Task - Schedule Interview  */
 const scheduleInterview = async (event) => {
     const count = event.count;
+    const isCandidateExist = event.isCandidateExist;
+    const message = "Scheduling Initiated";
     return {
-        message: "Scheduling Initiated",
+        message,
         count,
+        isCandidateExist,
     };
 };
 
 /** Lambda Task - Add Candidate */
 const addCandidate = async (event) => {
     const count = event.count;
+    const message = "Add Candidate Under Job";
+    const isCandidateExist = event.isCandidateExist;
     return {
-        message: "Add Candidate first",
+        message,
         count,
+        isCandidateExist,
     };
 };
 
