2c43f9297c4d2674513eb9a4ceb229db6268a024
sujan <sujanb.poojary@gmail.com>
2024-03-06 15:31:53 +0530

    Feat: Added choice state and made decision to flow the control

--- src/handler.js
+++ src/handler.js
@@ -1,7 +1,27 @@
 /** Lambda step function Task */
-module.exports.checkCandidate = async (event) => {
-  const candidateCount = 10;
-  return {
-    candidateCount,
-  };
+const checkCandidateAvailability = async (event) => {
+    const count = event.count;
+    return {
+        count,
+    };
+};
+
+/** Lambda Task - Schedule Interview  */
+const scheduleInterview = async (event) => {
+    return {
+        message: "Scheduling Initiated",
+    };
+};
+
+/** Lambda Task - Add Candidate */
+const addCandidate = async (event) => {
+    return {
+        message: "Add Candidate first",
+    };
+};
+
+module.exports = {
+    checkCandidateAvailability,
+    scheduleInterview,
+    addCandidate,
 };
